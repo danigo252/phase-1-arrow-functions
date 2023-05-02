7d820daa4cc6855dfb5b98ee106f1630d7acff00
daniel <danielgonzalez252@gmail.com>
2023-05-02 02:26:48 -0400

    Lab Completed

---
+++ index.js
@@ -0,0 +1,19 @@
+// Declaracion de function
+let div;
+function divide()
+{
+
+    return div = 2000/100;
+}
+
+
+//
+const square = (num) =>
+{
+    return num * num;
+}
+
+const add = (add1 , add2) =>
+{
+    return add1 + add2;
+}
